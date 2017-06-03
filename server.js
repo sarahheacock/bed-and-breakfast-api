971b971156a09a0b11394a3b01cefa2acf9e0697
sarahheacock <seheacock@bellsouth.net>
2017-06-03 15:27:05 -0400

    change header

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(function(req, res, next){
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   if(req.method === "OPTIONS"){
-    res.header("Access-Control-Allow-Methods", "PUT,POST,GET");
+    res.header("Access-Control-Allow-Methods", "PUT,POST");
     return res.status(200).json({});
   }
   next();
