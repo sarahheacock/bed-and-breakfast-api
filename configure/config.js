da9f62b40b1a9894aad121b64a65da96e79b9c97
sarahheacock <seheacock@bellsouth.net>
2017-06-13 15:32:31 -0400

    added admin editing

---
+++ configure/config.js
@@ -0,0 +1,14 @@
+// module.exports = {
+//   db: {
+//       production: "mongodb://"+process.env.MONGODB_ADDRESS+":ds117311/bed-and-breakfast-rooms",
+//       development: "mongodb://"+process.env.MONGODB_ADDRESS+":ds117311/bed-and-breakfast-rooms",
+//   }
+// };
+
+module.exports = {
+  'secret': 'ovzxujmsfdtjclcw',
+  'email': 'balanace.test@gmail.com',
+  'pass': 'GC9hXE6uQNJyMWJw',
+  'to': 'seheacock@bellsouth.net',
+  'database': 'mongodb://heroku_lt50lh59:13d6h6ct41aaqo63l2o3i9qulg@ds163301.mlab.com:63301/heroku_lt50lh59'
+}
